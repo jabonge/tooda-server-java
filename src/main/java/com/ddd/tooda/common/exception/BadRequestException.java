6e21004b8cce54ddde9c48ef094695afcae9e1bf
jaebong <yuwoqhd@gmail.com>
2021-12-15 23:41:03 +0900

    feat: 기본 생성자 추가

--- src/main/java/com/ddd/tooda/common/exception/BadRequestException.java
+++ src/main/java/com/ddd/tooda/common/exception/BadRequestException.java
@@ -4,4 +4,7 @@ public class BadRequestException extends RuntimeException{
     public BadRequestException(String msg) {
         super(msg);
     }
+    public BadRequestException() {
+        super("잘못된 요청 입니다.");
+    }
 }
