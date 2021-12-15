9a402278eb11cef60add96977d18bea2c6acf827
jaebong <yuwoqhd@gmail.com>
2021-12-15 23:39:55 +0900

    feat: NoHandlerFound Error 처리

--- src/main/java/com/ddd/tooda/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ddd/tooda/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 @Slf4j
@@ -21,6 +22,15 @@ public class GlobalExceptionHandler {
     public ApiError handleBadRequest(Exception e) {
         log.error(e.getMessage());
         return new ApiError(e,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {
+            NoHandlerFoundException.class,
+    })
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNotFoundRequest(Exception e) {
+        log.error(e.getMessage());
+        return new ApiError(e,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {
