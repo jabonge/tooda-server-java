4b48bbed815af647a08da4613ebf78b2f949e072
jaebong <yuwoqhd@gmail.com>
2021-12-18 22:59:45 +0900

    feat: Mysql 설정 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,1 @@
-insert into User (id, created_at, updated_at, device_id) values (1, now(), now(), 'ABCD_EFGWER_GWEERU_ABCD_EFGWER_GWEER');
+insert into user (id, created_at, updated_at, device_id) values (1, now(), now(), 'ABCD_EFGWER_GWEERU_ABCD_EFGWER_GWEER');
