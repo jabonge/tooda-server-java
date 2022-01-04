145065007ee5fdbd51618bd23970b06013b2c951
jaebong <yuwoqhd@gmail.com>
2022-01-04 13:29:11 +0900

    update: AmazonS3Client -> AmazonS3 type 변경

--- src/main/java/com/ddd/tooda/config/AmazonS3Config.java
+++ src/main/java/com/ddd/tooda/config/AmazonS3Config.java
@@ -2,7 +2,7 @@ package com.ddd.tooda.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,10 +21,10 @@ public class AmazonS3Config {
     private String region;
 
     @Bean
-    @Qualifier("S3Client")
-    public AmazonS3Client amazonS3Client() {
+    @Qualifier("S3")
+    public AmazonS3 amazonS3Client() {
         BasicAWSCredentials credential = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credential))
                 .build();
