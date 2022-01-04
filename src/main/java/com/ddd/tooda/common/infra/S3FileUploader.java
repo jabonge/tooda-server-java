7d0d808ddbac6cea74f5f4bd0e6e0f2e6727000a
jaebong <yuwoqhd@gmail.com>
2022-01-04 13:31:29 +0900

    fix: getObjectMetaData 추가

--- src/main/java/com/ddd/tooda/common/infra/S3FileUploader.java
+++ src/main/java/com/ddd/tooda/common/infra/S3FileUploader.java
@@ -1,13 +1,16 @@
 package com.ddd.tooda.common.infra;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.IOUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
@@ -16,14 +19,15 @@ import java.util.stream.Collectors;
 @Component
 public class S3FileUploader {
 
-    @Qualifier("S3Client")
-    private final AmazonS3Client s3Client;
+
+    private final AmazonS3 s3Client;
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;
     @Value("${spring.profiles.active}")
     private String profile;
 
-    public S3FileUploader(AmazonS3Client s3Client) {
+
+    public S3FileUploader(@Qualifier("S3") AmazonS3 s3Client) {
         this.s3Client = s3Client;
     }
 
@@ -33,14 +37,23 @@ public class S3FileUploader {
 
     private String putS3(MultipartFile multipartFile, String dirName) {
         String fileName = profile + "/" + dirName + "/" + UUID.randomUUID();
+
         try {
-            s3Client.putObject(new PutObjectRequest(bucket, fileName, multipartFile.getInputStream(), null).withCannedAcl(
+            byte[] bytes = IOUtils.toByteArray(multipartFile.getInputStream());
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+            s3Client.putObject(new PutObjectRequest(bucket, fileName, byteArrayInputStream, getMetadata(bytes)).withCannedAcl(
                     CannedAccessControlList.PublicRead
             ));
         } catch (IOException e) {
             throw new RuntimeException("파일 변환 오류: " + e.getLocalizedMessage());
         }
         return s3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private ObjectMetadata getMetadata(byte[] bytes) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(bytes.length);
+        return  objectMetadata;
     }
 
 }
