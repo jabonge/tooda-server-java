097f22ef28dd3b3a59e97f27c86823339fe81c0c
jaebong <yuwoqhd@gmail.com>
2022-01-22 14:52:45 +0900

    feat: github action 설정

---
+++ script/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ECR_REPOSITORY=$AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/tooda-test:latest
+#login
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr.ap-northeast-2.amazonaws.com
+# ecr pull
+docker pull "$ECR_REPOSITORY"
+# image rename
+docker tag "$ECR_REPOSITORY" tooda-app
+
+if [ "$( docker container inspect -f '{{.State.Status}}' tooda_spring )" == "running" ];then
+  docker stop tooda_spring
+  docker rm tooda_spring
+fi
+
+docker-compose up -d --build
+docker image prune -f
