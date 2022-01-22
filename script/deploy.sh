06b223dde94ba9c64f9bf422ea810f0bbffedf0d
jaebong <yuwoqhd@gmail.com>
2022-01-22 18:03:46 +0900

    update: deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+
+docker stop tooda_spring
+docker rm tooda_spring
+
 ECR_REPOSITORY=$AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/tooda-test:latest
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull "$ECR_REPOSITORY"
 docker tag "$ECR_REPOSITORY" tooda-app
-
-if [ "$( docker container inspect -f '{{.State.Status}}' tooda_spring )" == "running" ];then
-  docker stop tooda_spring
-  docker rm tooda_spring
-fi
 
 docker-compose up -d --build
 docker image prune -f
