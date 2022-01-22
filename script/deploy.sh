70b20e6824b6f79776230907f5dea0da7fd91e68
jaebong <yuwoqhd@gmail.com>
2022-01-22 18:22:56 +0900

    update: deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ECR_REPOSITORY=$AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/tooda-test:latest
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr.ap-northeast-2.amazonaws.com
+ECR_REPOSITORY=485616710973.dkr.ecr.ap-northeast-2.amazonaws.com/tooda-test:latest
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 485616710973.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull "$ECR_REPOSITORY"
 docker tag "$ECR_REPOSITORY" tooda-app
 
