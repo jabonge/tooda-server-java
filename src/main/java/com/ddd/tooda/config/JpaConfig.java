ac0060a4d8c0630b9f130745d2dc92b13ccde766
jaebong <yuwoqhd@gmail.com>
2022-02-06 16:46:21 +0900

    update: 한국 시간으로 createAt, updatedAt 변경

--- src/main/java/com/ddd/tooda/config/JpaConfig.java
+++ src/main/java/com/ddd/tooda/config/JpaConfig.java
@@ -1,9 +1,19 @@
 package com.ddd.tooda.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
 public class JpaConfig {
+    @Bean
+    public DateTimeProvider utcDateTimeProvider() {
+        return () -> Optional.of(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+    }
 }
